be33c2bbeed57c766febec3e4b2523f4a3a0ab31
Teemu Perämäki <teemu.peramaki@icloud.com>
2021-12-01 09:49:05 +0200

    Day 1

---
+++ 2021/1a.py
@@ -0,0 +1,12 @@
+data = open('2021/data/1a.txt', 'rt')
+
+count = -1
+prev = 0
+
+for xString in data:
+  x = int(xString)
+  if x > prev:
+    count += 1
+  prev = x
+
+print(count)
---
+++ 2021/1b.py
@@ -0,0 +1,16 @@
+data = open('2021/data/1a.txt', 'rt')
+
+count = -1
+prev = 0
+
+dataList = list(map(lambda x: int(x), data.read().splitlines()))
+
+for index, number in enumerate(dataList):
+  if index < 1 or index >= len(dataList) - 1:
+    continue
+  sum = dataList[index - 1] + number + dataList[index + 1]
+  if sum > prev:
+    count += 1
+  prev = sum
+
+print(count)
